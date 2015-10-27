d4c9e93e858c50b46941405dc353b2323df8b655
sam <sha.sam@inventec.com.cn>
2015-10-27 11:56:34 +0800

    CopyWithQuotes

---
+++ CopyWithQuotes.py
@@ -0,0 +1,27 @@
+import sublime
+import sublime_plugin
+
+class cwq0(sublime_plugin.TextCommand):
+    #Convert Tabs To Spaces
+    def run(self, edit):
+        sublime.status_message('cwq0')
+
+class cwq1(sublime_plugin.TextCommand):
+    #Convert Tabs To Spaces
+    def run(self, edit):
+        sublime.status_message('cwq1')
+
+class cwq2(sublime_plugin.TextCommand):
+    #Convert Tabs To Spaces
+    def run(self, edit):
+        sublime.status_message('cwq2')
+
+class cwq3(sublime_plugin.TextCommand):
+    #Convert Tabs To Spaces
+    def run(self, edit):
+        sublime.status_message('cwq3')
+
+class cwq4(sublime_plugin.TextCommand):
+    #Convert Tabs To Spaces
+    def run(self, edit):
+        sublime.status_message('cwq4')
