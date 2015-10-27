f8043e7a1e6c6f3d1b999ac6749e34ec54ff7705
sam <sha.sam@inventec.com.cn>
2015-10-27 19:59:05 +0800

    support Java/C#

--- CopyWithQuotes.py
+++ CopyWithQuotes.py
@@ -1,27 +1,63 @@
 import sublime
 import sublime_plugin
+import re
 
-class cwq0(sublime_plugin.TextCommand):
-    #Convert Tabs To Spaces
-    def run(self, edit):
-        sublime.status_message('cwq0')
 
-class cwq1(sublime_plugin.TextCommand):
-    #Convert Tabs To Spaces
+class CwqJavaCsharpCommand(sublime_plugin.TextCommand):
+    # Convert Tabs To Spaces
     def run(self, edit):
-        sublime.status_message('cwq1')
+        for sel_region in self.view.sel():
+            if not sel_region.empty():
+                sel_text = self.view.substr(sel_region)
+                q_text = quotes_java_csharp(sel_text)
+                sublime.set_clipboard(q_text)
+                sublime.status_message("quotes for Java/C# !")
+            else:
+                sublime.status_message("quotes error !")
+        return
 
-class cwq2(sublime_plugin.TextCommand):
-    #Convert Tabs To Spaces
-    def run(self, edit):
-        sublime.status_message('cwq2')
 
-class cwq3(sublime_plugin.TextCommand):
-    #Convert Tabs To Spaces
+class CwqCppCommand(sublime_plugin.TextCommand):
+    # Convert Tabs To Spaces
     def run(self, edit):
-        sublime.status_message('cwq3')
+        sublime.status_message("not support C/C++ !")
+        return
 
-class cwq4(sublime_plugin.TextCommand):
-    #Convert Tabs To Spaces
+
+class CwqPhpPerlCommand(sublime_plugin.TextCommand):
+    # Convert Tabs To Spaces
     def run(self, edit):
-        sublime.status_message('cwq4')
+        sublime.status_message("not support PHP/Perl !")
+        return
+
+
+class CwqDelphiCommand(sublime_plugin.TextCommand):
+    # Convert Tabs To Spaces
+    def run(self, edit):
+        sublime.status_message("not support Delphi !")
+        return
+
+
+class CwqObjcCommand(sublime_plugin.TextCommand):
+    # Convert Tabs To Spaces
+    def run(self, edit):
+        sublime.status_message("not support Obj-c !")
+        return
+
+
+def quotes_java_csharp(str):
+    sep = "\n"
+    m1 = re.search("\r\n", str)
+    if m1:
+        sep = "\r\n"
+    lines = re.split("\r\n|\n", str)
+    content = ""
+    count = 0
+    for line in lines:
+        line = line.replace('"', '\\"')
+        if (count < len(lines) - 1):
+            content += '"' + line + '" + ' + sep
+        else:
+            content += '"' + line + '";'
+        count += 1
+    return content
---
+++ README.md
@@ -0,0 +1,13 @@
+Context.sublime-menu:鼠标右键上下文菜单。
+
+Default (Windows).sublime-keymap：键盘映射。
+
+CopyWithQuotes.py：插件源代码
+
+Default.sublime-commands：命令定义
+
+Main.sublime-menu：功能菜单（可以不要）
+
+package-metadata.json：不确定是否必须
+
+packages.json：仓库必须
